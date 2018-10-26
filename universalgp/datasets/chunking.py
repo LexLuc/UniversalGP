22b763563f3b9c9751a270dcbff3774b5cbbd38c
LexLuc <lexluc.lei@gmail.com>
2018-10-26 19:30:35 +1100

    fix non-exist path

--- universalgp/datasets/chunking.py
+++ universalgp/datasets/chunking.py
@@ -21,8 +21,8 @@ def chunking(_):
     """
     num_inducing = 100
     print('loading data...')
-    X = np.load('./universalgp/datasets/data/chunking/train_x.npz')
-    Y = np.load('./universalgp/datasets/data/chunking/train_y.npz')
+    X = np.load('./universalgp/datasets/data/chunking/x_train.npy')
+    Y = np.load('./universalgp/datasets/data/chunking/y_train.npy')
     print(f'data loaded: shape of x = {X.shape}, shape of y = {Y.shape}')
 
 #    X = np.linspace(0, 5, num=300, endpoint=False, dtype=np.float32)[:, np.newaxis]    # toy dataset
